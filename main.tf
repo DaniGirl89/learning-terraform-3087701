c1d952a04452ab0de988df8543562a61a39c7799
DaniGirl89 <41187883+DaniGirl89@users.noreply.github.com>
2025-09-19 15:01:03 +0100

    fixing typo

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ resource "aws_security_group" "blog" {
   name        = "blog"
   description = "Allow HTTP & HTTPS in. Allow Everything Out"
 
-  vpc_id = data.aws.aws_vpc.default.id
+  vpc_id = data.aws_vpc.default.id
 }
 
 resource "aws_security_group_rule" "blog_http_in" {
